54174db7e7a0e9a45307b31fafb70ec8f352eba3
Sam Kowitch <sammyjkow1@gmail.com>
2024-04-08 18:58:06 -0700

    Lesson 2 Complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import SunAndMoon from "./SunAndMoon/SunAndMoon.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>Sam Kowitch</React.StrictMode>
+    <React.StrictMode>
+        <SunAndMoon />
+    </React.StrictMode>
 );
